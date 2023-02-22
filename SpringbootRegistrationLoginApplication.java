8caffd9acdfc165a2a60a16c2dd9b42dc3adba04
Himanshu Devi <himanshu2devi@gmail.com>
2023-02-22 19:30:04 +0530

    Add files via upload

---
+++ SpringbootRegistrationLoginApplication.java
@@ -0,0 +1,13 @@
+package com.himanshu.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRegistrationLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRegistrationLoginApplication.class, args);
+	}
+
+}
